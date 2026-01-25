98c373313f8f39d93d23e284edd116432c493e40
Saaappi <alex.metz1024@gmail.com>
2026-01-24 23:45:07 -0800

    Wrote a skill level getter but I'm too tired to continue

--- Services/CraftRunner.lua
+++ Services/CraftRunner.lua
@@ -16,3 +16,11 @@ CraftRunner._maxNoSkillupCasts = 8
 
 CraftRunner._recipeID = nil
 CraftRunner._recipeSpellID = nil
+
+local function GetCurrentSkillLevel()
+  if Professions and Professions.GetProfessionInfo then
+    local info = Professions.GetProfessionInfo()
+    return (info and info.skillLevel) or 0
+  end
+  return 0
+end
